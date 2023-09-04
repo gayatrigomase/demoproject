0736de0d495dbe79f57ffb6ce0eecfaaf36f57d2
gayatrigomase <g.gomase123@gmail.com>
2023-09-04 12:45:09 +0530

    added

---
+++ src/main/java/com/example/demoproject/hotel.java
@@ -0,0 +1,12 @@
+package com.example.demoproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class hotel {
+    @GetMapping("/hotel")
+    public String getData(){
+        return "Book your hejhjejfejw safe stay with us!!!!!";
+    }
+
+}
